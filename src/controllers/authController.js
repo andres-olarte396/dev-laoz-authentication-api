a1224b8321f9a01fbc1a3dea2d56e74a4ab8dcae
andres-olarte396 <andres.olarte396@gmail.com>
2024-12-28 01:19:02 -0500

    :sparkles: Agregar manejo de sesiones: implementar modelo de sesión y generar tokens únicos para la autenticación de usuarios.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const crypto = require('crypto');
+const Session = require('../models/Session');
+const User = require('../models/User'); 
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
@@ -10,19 +12,31 @@ const loginUser = async (req, res) => {
 
   try {
     const user = await User.findOne({ username });
-    if (user && (await user.matchPassword(password))) {
 
-      return res.status(200).json({
-        _id: user.id,
-        username: user.username,
-        role: user.role,
-        permissions: user.permissions,
-        token: generateToken(user.id),
-      });
-    } else {
+    if (!user || !(await user.matchPassword(password))) {
       return res.status(401).json({ error: "Invalid credentials" });
     }
+
+    // Generar un token único para la sesión
+    const sessionToken = crypto.randomBytes(64).toString("hex");
+
+    // Definir la caducidad de la sesión (por ejemplo, 1 hora)
+    const expiresAt = new Date(Date.now() + 60 * 60 * 1000);
+
+    // Crear y guardar la sesión en la base de datos
+    const session = new Session({
+      sessionToken,
+      userId: user._id,
+      expiresAt
+    });
+
+    await Session.create(session);
+
+    return res.status(200).json({
+      token: generateToken(sessionToken),
+    });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Server error" });
   }
 };
